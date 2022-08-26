d54c01ff0fe82bb6d675e0f4871a2a116adca2ff
Walidoux <ma.walidkorchi@icloud.com>
2022-08-26 17:36:03 +0100

    refactor: 🔧 implement parser options

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 module.exports = {
   extends: ['conventions', 'prettier'],
   plugins: ['prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    project: './tsconfig.json'
+  },
   env: {
     node: true,
     browser: true
